0d98d9167f1c8b4a63f37fcb0988e1425df52ce7
Ethertrust <egoshvedo@mail.ru>
2017-02-17 12:00:24 +0500

    FQW1

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
 /**
  * Created by shvedov_es on 15.02.2017.
  */
+
+$(document).ready(function(){
+
+    $( "#cboxCuratorAdd" ).click(function() {
+        $('#curators').prepend($('#webform-component-curator').clone());
+    });
+
+    $('.webform-client-form').on('submit', function(event) {
+        event.preventDefault();
+        var topic = $.trim($('#edit-submitted-title').val());
+        var submittedfio = $.trim($('#edit-submitted-fio').val());
+        var submittedstudwork = $.trim($('#edit-submitted-year').val());
+        var submittedstudVKR = $.trim($('#edit-submitted-VKR').val());
+        var submittedstudCurator = findMultiID('edit-submitted-curator');
+        var submittedstudKod = $.trim($('#edit-submitted-kod').val());
+        var submittedstudInstitute = $.trim($('#edit-submitted-institute').val());
+        var submittedstudPages = $.trim($('#edit-submitted-pages').val());
+        var submittedvash_email = $.trim($('#edit-submitted-vash-email').val());
+        var submittedPDF = $.trim($('#fileToUpload').val());
+        var allright = true;
+        if (topic != "Запись on-line") {
+            allright = false;
+        }
+        if (!valid(submittedfio)) {
+            $('#error1').css("display", "block");
+            $('#edit-submitted-fio').css("border", "1px solid #d11313");
+            allright = false;
+        }
+        if (!valid(submittedvash_email, true)) {
+            $('#ok1').css("display", "none");
+            $('#notok1').css("display", "block");
+            $('#edit-submitted-vash-email').css("border", "1px solid #d11313");
+            allright = false;
+        }
+        if (!valid(submittedpn)) {
+            $('#error2').css("display", "block");
+            $('#edit-submitted-pn').css("border", "1px solid #d11313");
+            allright = false;
+        }
+        if (!valid(submittedstudwork)) {
+            $('#error4').css("display", "block");
+            $('#edit-submitted-studwork').css("border", "1px solid #d11313");
+            allright = false;
+        }
+        if (!valid(submittedbirthdate)) {
+            $('#error5').css("display", "block");
+            $('#edit-submitted-birthdate').css("border", "1px solid #d11313");
+            allright = false;
+        }
+
+        if (!allright)
+            return;
+    });
+ }
+)
+
+function findMultiID(id) {
+    var results = [];
+    var children = $("div").get(0).children;
+    for (var i = 0; i < children.length; i++) {
+        if (children[i].id == id) {
+            results.push(children[i].val());
+        }
+    }
+    return(results);
+}
+
+//text - string value, ismail - bool value and is false by default
+function valid(text,ismail)
+{
+    ismail = typeof ismail != 'undefined' ? ismail : false;
+    if(ismail)
+        return /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/.test(text);
+    else
+        return !/^[ \t\r\n\v\f]*$/.test(text);
+}
--- webformFQW.php
+++ webformFQW.php
@@ -1,13 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $array = require_once('config.php');
-
-while ($fruit_name = current($array)) {
-    if ($fruit_name == 'apple') {
-        echo key($array).'<br />';
-    }
-    next($array);
-}
 ?>
 <html>
 <head>
@@ -32,50 +25,64 @@ while ($fruit_name = current($array)) {
     </div>
     <div class="form-item webform-component webform-component-textfield" id="webform-component-year">
         <label for="edit-submitted-year">Год написания <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <input type="text" id="edit-submitted-year" name="submittedyear" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
+        <input type="date" id="edit-submitted-year" name="submittedyear" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
         <label for="edit-submitted-year" id="error1" class="error" style="display: none;">заполните, пожалуйста</label>
     </div>
     <div class="form-item webform-component webform-component-selectfield" id="webform-component-VKR">
         <label for="edit-submitted-VKR">Форма ВКР <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <select name="VKR[]">
+        <select name="VKR[]" id="#edit-submitted-VKR">
             <option selected="selected" disabled>Форма ВКР </option>
+            <?php
+            while ($fruit_name = current($array["work_type"])) {
+                echo '<option>'.htmlspecialchars($fruit_name).'</option>';
+                next($array["work_type"]);
+            }
+            ?>
         </select>
         <label for="edit-submitted-VKR" id="error1" class="error" style="display: none;">выберите, пожалуйста</label>
     </div>
-    <div class="form-item webform-component webform-component-textfield" id="webform-component-curator">
-        <label for="edit-submitted-curator">Научный руководитель (Фамилия, Имя, Отчество - полностью) <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <input type="text" id="edit-submitted-curator" name="submittedcurator" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
-        <label for="edit-submitted-curator" id="error1" class="error" style="display: none;">заполните, пожалуйста</label>
+    <div id="curators">
+        <div class="form-item webform-component webform-component-textfield" id="webform-component-curator">
+            <label for="edit-submitted-curator">Научный руководитель (Фамилия, Имя, Отчество - полностью) <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
+            <input type="text" id="edit-submitted-curator" name="submittedcurator" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
+            <label for="edit-submitted-curator" id="error1" class="error" style="display: none;">заполните, пожалуйста</label>
+        </div>
+        <button type="button" id="cboxCuratorAdd">Добавить руководителя</button>
     </div>
-    <button type="button" id="cboxCuratorAdd">Добавить руководителя</button>
     <div class="form-item webform-component webform-component-textfield" id="webform-component-kod">
         <label for="edit-submitted-kod">ФИО <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <input type="text" id="edit-submitted-kod" name="submittedfio" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
+        <input type="text" id="edit-submitted-kod" name="submittedkod" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
         <label for="edit-submitted-kod" id="error1" class="error" style="display: none;">заполните, пожалуйста</label>
     </div>
     <div class="form-item webform-component webform-component-selectfield" id="webform-component-institutes">
-        <label for="edit-submitted-institutes">Научный руководитель (Фамилия, Имя, Отчество - полностью) <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <select name="institutes[]">
+        <label for="edit-submitted-institutes">Выберите институт <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
+        <select name="institutes[]" id="#edit-submitted-institute">
             <option selected="selected" disabled>Выберите институт</option>
+            <?php
+            while ($fruit_name = current($array["institute"])) {
+                echo '<option>'.htmlspecialchars(key($array["institute"])).'</option>';
+                next($array["institute"]);
+            }
+            ?>
         </select>
         <label for="edit-submitted-institutes" id="error1" class="error" style="display: none;">выберите, пожалуйста</label>
     </div>
     <div class="form-item webform-component webform-component-textfield" id="webform-component-pages">
         <label for="edit-submitted-pages">К-во страниц <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <input type="text" id="edit-submitted-pages" name="submittedfio" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
+        <input type="text" id="edit-submitted-pages" name="submittedpages" value="" size="60" maxlength="128" class="form-text required" style="width:213px;">
         <label for="edit-submitted-pages" id="error1" class="error" style="display: none;">заполните, пожалуйста</label>
     </div>
     <div class="form-item webform-component webform-component-email" id="webform-component-vash-email">
         <label for="edit-submitted-vash-email">Действующий адерс эл. почты <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
         <input class="email form-text form-email required" type="email" id="edit-submitted-vash-email" name="submittedvash_email" size="60" style="width:213px; float:left;">
-        <img id="ok1" src="../images/ok.jpg" alt="ok" style="display:none;">
-        <img id="notok1" src="../images/notok.jpg" alt="error" style="display:none;">
+        <img id="ok" src="images/ok.jpg" alt="ok" style="display:none;">
+        <img id="notok" src="images/notok.jpg" alt="error" style="display:none;">
     </div>
     <div class="form-item webform-component webform-component-email" id="webform-component-vash-pdf">
         <label for="edit-submitted-vash-pdf">Присоединить текст работы (формат PDF) <span class="form-required" title="Это поле обязательно для заполнения.">*</span></label>
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <img id="ok1" src="../images/ok.jpg" alt="ok" style="display:none;">
-        <img id="notok1" src="../images/notok.jpg" alt="error" style="display:none;">
+        <input type="file" name="fileToUpload" id="fileToUpload" accept=".pdf">
+        <img id="ok1" src="images/ok.jpg" alt="ok" style="display:none;">
+        <img id="notok1" src="images/notok.jpg" alt="error" style="display:none;">
     </div>
  <p><input type="submit" /></p>
 </form>
